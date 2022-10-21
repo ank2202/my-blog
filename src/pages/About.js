5d8935b473f8e4dabb7dd656e5f2e0795df52b0b
ank2202 <116373360+ank2202@users.noreply.github.com>
2022-10-21 21:36:45 +0530

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <div className="text-center">
         <h1 className="text-3xl">About Me</h1>
-        <p className="text-xl">This is a sample blog built with React and Tailwind CSS.</p>
+        <p className="text-xl">This is about me page !!!</p>
         </div>
     );
     }
